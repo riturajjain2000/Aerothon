008d17500ded1c8f8797019205804607ba478af7
Rituraj Jain <rrjain20122000@gmail.com>
2023-05-13 10:32:31 +0530

    Added buy sell forms

--- src/components/Airline/Dashboard.js
+++ src/components/Airline/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Search from "./Search";
 import Metrics from "./Metrics";
+import SellForm from "./SellForm";
 
 function Dashboard(props) {
   return (
     <div>
+      <SellForm/>
       <Search props={props} />
       <Metrics />
     </div>
--- src/components/Recycler/Dashboard.js
+++ src/components/Recycler/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Search from "./Search";
 import Metrics from "./Metrics";
+import BuyList from "./BuyList";
 
 function Dashboard() {
   return (
     <div>
+      <BuyList/>
       <Search />
       <Metrics />
     </div>
